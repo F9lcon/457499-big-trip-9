e22d4683a11a504a312623736b41d279c1e66e61
f9lcon <twisy12@mail.ru>
2019-08-11 14:31:18 +0300

    Отрисовка компонентов. Настройка webpack

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import {createTripInfo} from "./components/trip-info";
+import {createMenu} from "./components/menu";
+import {createFilter} from "./components/filter";
+import {createSortForm} from "./components/sort-form";
+import {createTripEditForm} from "./components/trip-edit";
+import {createTripList} from "./components/trip-list";
+import {createTrip} from "./components/trip";
+
+const tripInfoElement = document
+  .querySelector(`.trip-main__trip-info.trip-info`);
+const menuHeaderElement = document
+  .querySelector(`.trip-main__trip-controls h2`);
+const tripControlsElement = document
+  .querySelector(`.trip-main__trip-controls.trip-controls`);
+const eventsElement = document.querySelector(`.trip-events`);
+
+const renderComponent = (element, component, position = `beforeend`) => {
+  element.insertAdjacentHTML(position, component);
+};
+
+const renderContent = () => {
+  const tripListElement = document.querySelector(`.trip-events__list`);
+  for (let i = 0; i < 3; i++) {
+    renderComponent(tripListElement, createTrip());
+  }
+};
+
+const renderAllComponents = () => {
+  renderComponent(tripInfoElement, createTripInfo(), `afterbegin`);
+  renderComponent(menuHeaderElement, createMenu(), `afterend`);
+  renderComponent(tripControlsElement, createFilter());
+  renderComponent(eventsElement, createSortForm());
+  renderComponent(eventsElement, createTripEditForm());
+  renderComponent(eventsElement, createTripList());
+  renderContent();
+};
+
+renderAllComponents();
+
