7ef18da7abb05b6860400af5fc28f7672762f366
f9lcon <twisy12@mail.ru>
2019-09-22 15:28:59 +0300

    Личный проект: обратная связь

--- src/components/trip-list.js
+++ src/components/trip-list.js
@@ -1,8 +1,10 @@
 import AbstractComponent from "./abstract-component";
 
 export default class TripList extends AbstractComponent {
-  constructor() {
+  constructor(date, isEventListEmpty) {
     super();
+    this._date = date;
+    this._isEventListEmpty = isEventListEmpty;
   }
 
   getTemplate() {
@@ -10,8 +12,8 @@ export default class TripList extends AbstractComponent {
   <ul class="trip-days">
     <li class="trip-days__item  day">
       <div class="day__info">
-        <span class="day__counter">1</span>
-        <time class="day__date" datetime="2019-03-18">MAR 18</time>
+        <span class="day__counter">${this._isEventListEmpty ? `` : 1}</span>
+        <time class="day__date" datetime="2019-03-18"> ${this._date} </time>
       </div>
       <ul class="trip-events__list">
       </ul>
